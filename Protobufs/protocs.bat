23074296c7342c9d1ccd9731415a8871d0f0bebf
Mads <madslind@gmail.com>
2017-07-02 13:43:33 +0200

    protobuf setup

---
+++ Protobufs/protocs.bat
@@ -0,0 +1,6 @@
+protoc -I=$SRC_DIR --csharp_out=$DST_DIR $SRC_DIR/addressbook.proto
+
+
+//C#
+protoc -I=C:\Users\torybash\Documents\Prolab\Prolab\ProlabServer\ProlabServer\Protobufs --csharp_out=C:\Users\torybash\Documents\Prolab\Prolab\ProlabServer\ProlabServer\Protobufs C:\Users\torybash\Documents\Prolab\Prolab\ProlabServer\ProlabServer\Protobufs\ProlabBuf.proto
+//JS
